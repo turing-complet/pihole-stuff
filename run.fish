7f282d67e4fe2467196509c4cb90521bd7557f5c
jon <jhagg314@gmail.com>
2019-08-31 00:04:32 -0700

    initial commit

---
+++ run.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+
+systemctl is-active --quiet systemd-resolved.service
+if test $status -eq 0
+    echo "The systemd resolver is active. Disabling it just in case."
+    echo "To re-enable it, run this:"
+    echo "sudo systemctl enable systemd-resolved"
+    sudo systemctl stop systemd-resolved
+end
+
+sudo systemctl stop systemd-resolved 
+
+set resolvconf (cat /etc/resolv.conf)
+
+if not grep -q "127.0.0.1" /etc/resolv.conf
+    echo "Adding localhost to resolv.conf"
+    sudo echo "nameserver 127.0.0.1" > /etc/resolv.conf
+end
+
+set container (docker inspect -f '{{.State.Running}}' pihole)
+
+if [ $container != "true" ]
+    echo "Pihole is not running. Starting it now."
+    docker-compose up
+end
