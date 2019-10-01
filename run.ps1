68a42d3e5d6dc800fdf9abe3ddba1365352f1f1d
root <jhagg314@gmail.com>
2019-09-30 18:38:34 -0700

    verbose switch

--- run.ps1
+++ run.ps1
@@ -1,23 +1,25 @@
+[CmdletBinding()]
+param()
 
 systemctl is-active --quiet systemd-resolved.service
 
 if ($LASTEXITCODE -eq 0) {
-    Write-Host "The systemd resolver is active. Disabling it just in case."
-    Write-Host "To re-enable it, run this:"
-    Write-Host "sudo systemctl enable systemd-resolved"
+    Write-Verbose "The systemd resolver is active. Disabling it just in case."
+    Write-Verbose "To re-enable it, run this:"
+    Write-Verbose "sudo systemctl enable systemd-resolved"
     sudo systemctl stop systemd-resolved
 }
 
 $match = type /etc/resolv.conf | Select-String -Pattern "127.0.0.1"
 
 if (!$match) {
-    Write-Host "Adding localhost to resolv.conf"
+    Write-Verbose "Adding localhost to resolv.conf"
     Set-Content -Path /etc/resolv.conf -Value "nameserver 127.0.0.1"
 }
 
 $container = docker inspect -f '{{.State.Running}}' pihole
 
 if ($container -ne "true") {
-    Write-Host "Pihole is not running. Starting it now."
+    Write-Verbose "Pihole is not running. Starting it now."
     docker-compose up -d
 }
